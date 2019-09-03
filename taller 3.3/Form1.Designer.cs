da16ce5fa6c08215964f38e7dd6b625152b6deba
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-09-03 17:35:34 -0500

    punto 3 del taller 3,mayor y menor

---
+++ taller 3.3/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace taller_3._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.lblmayor = new System.Windows.Forms.Label();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lblmenor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(412, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificardor de mayores y menores";
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblnumero1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblnumero1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero1.Location = new System.Drawing.Point(64, 139);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(166, 25);
+            this.lblnumero1.TabIndex = 1;
+            this.lblnumero1.Text = "Ingrese un número";
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblnumero2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblnumero2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero2.Location = new System.Drawing.Point(64, 206);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(180, 25);
+            this.lblnumero2.TabIndex = 2;
+            this.lblnumero2.Text = "Ingrese otro número";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero1.Location = new System.Drawing.Point(265, 138);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(100, 29);
+            this.txtnumero1.TabIndex = 3;
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero2.Location = new System.Drawing.Point(265, 205);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(100, 29);
+            this.txtnumero2.TabIndex = 4;
+            // 
+            // lblmayor
+            // 
+            this.lblmayor.AutoSize = true;
+            this.lblmayor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblmayor.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmayor.Location = new System.Drawing.Point(246, 364);
+            this.lblmayor.Name = "lblmayor";
+            this.lblmayor.Size = new System.Drawing.Size(87, 29);
+            this.lblmayor.TabIndex = 5;
+            this.lblmayor.Text = " mayor ";
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.BackColor = System.Drawing.SystemColors.Info;
+            this.btnenviar.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(470, 151);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(95, 65);
+            this.btnenviar.TabIndex = 6;
+            this.btnenviar.Text = "Enviar";
+            this.btnenviar.UseVisualStyleBackColor = false;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lblmenor
+            // 
+            this.lblmenor.AutoSize = true;
+            this.lblmenor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblmenor.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmenor.Location = new System.Drawing.Point(583, 356);
+            this.lblmenor.Name = "lblmenor";
+            this.lblmenor.Size = new System.Drawing.Size(87, 29);
+            this.lblmenor.TabIndex = 7;
+            this.lblmenor.Text = " menor ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 369);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "el numero mayor es:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(417, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "el numero menor es:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblmenor);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.lblmayor);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblnumero1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Label lblmayor;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lblmenor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ taller 3.3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller_3._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = int.Parse(txtnumero1.Text);
+            num2 = int.Parse(txtnumero2.Text);
+
+            if (num1 > num2)
+            {
+
+                lblmayor.Text = num1.ToString();
+                lblmenor.Text = num2.ToString();
+
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+            }
+            else
+             if(num2>num1)
+            {
+
+                lblmayor.Text = num2.ToString();
+                lblmenor.Text = num1.ToString();
+
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+            }
+             else
+                  if (num1 == num2)
+            {
+
+                MessageBox.Show("Ambos numeros son iguales.");
+
+                lblmayor.Visible = false;
+                lblmenor.Visible = false;
+
+            }
+
+
+
+
+        }
+
+        
+    }
+
+}
